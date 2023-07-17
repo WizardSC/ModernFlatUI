f67248de01bcc7e46aa0a227417ff0cf4ac15487
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-07-17 12:20:51 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ModernFlatUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnSideMenu = new Panel();
+            panel1 = new Panel();
+            button1 = new Button();
+            panel2 = new Panel();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            pnSideMenu.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnSideMenu
+            // 
+            pnSideMenu.BackColor = Color.FromArgb(11, 7, 17);
+            pnSideMenu.Controls.Add(panel2);
+            pnSideMenu.Controls.Add(button1);
+            pnSideMenu.Controls.Add(panel1);
+            pnSideMenu.Dock = DockStyle.Left;
+            pnSideMenu.Location = new Point(0, 0);
+            pnSideMenu.Name = "pnSideMenu";
+            pnSideMenu.Size = new Size(262, 953);
+            pnSideMenu.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(262, 125);
+            panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Top;
+            button1.Location = new Point(0, 125);
+            button1.Name = "button1";
+            button1.Size = new Size(262, 45);
+            button1.TabIndex = 0;
+            button1.Text = "Quản lý";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(35, 32, 39);
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button2);
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 170);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(262, 165);
+            panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Top;
+            button2.Location = new Point(0, 0);
+            button2.Name = "button2";
+            button2.Size = new Size(262, 40);
+            button2.TabIndex = 0;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Dock = DockStyle.Top;
+            button3.Location = new Point(0, 40);
+            button3.Name = "button3";
+            button3.Size = new Size(262, 40);
+            button3.TabIndex = 1;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Dock = DockStyle.Top;
+            button4.Location = new Point(0, 80);
+            button4.Name = "button4";
+            button4.Size = new Size(262, 40);
+            button4.TabIndex = 2;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Dock = DockStyle.Top;
+            button5.Location = new Point(0, 120);
+            button5.Name = "button5";
+            button5.Size = new Size(262, 40);
+            button5.TabIndex = 3;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1582, 953);
+            Controls.Add(pnSideMenu);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            pnSideMenu.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnSideMenu;
+        private Button button1;
+        private Panel panel1;
+        private Panel panel2;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace ModernFlatUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
