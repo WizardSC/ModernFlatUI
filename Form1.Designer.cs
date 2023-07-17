d80715e6ee8c630936ceaa75be7df055254580e1
Huỳnh Phúc Toàn <97122589+WizardSC@users.noreply.github.com>
2023-07-17 22:39:38 +0700

    show/hide submenu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,99 +29,254 @@
         private void InitializeComponent()
         {
             pnSideMenu = new Panel();
-            panel1 = new Panel();
-            button1 = new Button();
-            panel2 = new Panel();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
+            pnQuanLySubMenu = new Panel();
+            button11 = new Button();
+            button10 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            btnQuanLy = new Button();
+            pnNhapHangSubMenu = new Panel();
             button5 = new Button();
+            button6 = new Button();
+            btnNhapHang = new Button();
+            pnBanHangSubMenu = new Panel();
+            button3 = new Button();
+            button2 = new Button();
+            btnBanHang = new Button();
+            panel1 = new Panel();
             pnSideMenu.SuspendLayout();
-            panel2.SuspendLayout();
+            pnQuanLySubMenu.SuspendLayout();
+            pnNhapHangSubMenu.SuspendLayout();
+            pnBanHangSubMenu.SuspendLayout();
             SuspendLayout();
             // 
             // pnSideMenu
             // 
+            pnSideMenu.AutoScroll = true;
             pnSideMenu.BackColor = Color.FromArgb(11, 7, 17);
-            pnSideMenu.Controls.Add(panel2);
-            pnSideMenu.Controls.Add(button1);
+            pnSideMenu.Controls.Add(pnQuanLySubMenu);
+            pnSideMenu.Controls.Add(btnQuanLy);
+            pnSideMenu.Controls.Add(pnNhapHangSubMenu);
+            pnSideMenu.Controls.Add(btnNhapHang);
+            pnSideMenu.Controls.Add(pnBanHangSubMenu);
+            pnSideMenu.Controls.Add(btnBanHang);
             pnSideMenu.Controls.Add(panel1);
             pnSideMenu.Dock = DockStyle.Left;
             pnSideMenu.Location = new Point(0, 0);
             pnSideMenu.Name = "pnSideMenu";
-            pnSideMenu.Size = new Size(262, 953);
+            pnSideMenu.Size = new Size(250, 953);
             pnSideMenu.TabIndex = 0;
+            // 
+            // pnQuanLySubMenu
+            // 
+            pnQuanLySubMenu.BackColor = Color.FromArgb(35, 32, 39);
+            pnQuanLySubMenu.Controls.Add(button11);
+            pnQuanLySubMenu.Controls.Add(button10);
+            pnQuanLySubMenu.Controls.Add(button8);
+            pnQuanLySubMenu.Controls.Add(button9);
+            pnQuanLySubMenu.Dock = DockStyle.Top;
+            pnQuanLySubMenu.Location = new Point(0, 450);
+            pnQuanLySubMenu.Name = "pnQuanLySubMenu";
+            pnQuanLySubMenu.Size = new Size(250, 173);
+            pnQuanLySubMenu.TabIndex = 5;
+            // 
+            // button11
+            // 
+            button11.Dock = DockStyle.Top;
+            button11.FlatAppearance.BorderSize = 0;
+            button11.FlatStyle = FlatStyle.Flat;
+            button11.ForeColor = Color.LightGray;
+            button11.Location = new Point(0, 120);
+            button11.Name = "button11";
+            button11.Padding = new Padding(35, 0, 0, 0);
+            button11.Size = new Size(250, 40);
+            button11.TabIndex = 3;
+            button11.Text = "button11";
+            button11.TextAlign = ContentAlignment.MiddleLeft;
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Dock = DockStyle.Top;
+            button10.FlatAppearance.BorderSize = 0;
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.ForeColor = Color.LightGray;
+            button10.Location = new Point(0, 80);
+            button10.Name = "button10";
+            button10.Padding = new Padding(35, 0, 0, 0);
+            button10.Size = new Size(250, 40);
+            button10.TabIndex = 2;
+            button10.Text = "button10";
+            button10.TextAlign = ContentAlignment.MiddleLeft;
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Dock = DockStyle.Top;
+            button8.FlatAppearance.BorderSize = 0;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.ForeColor = Color.LightGray;
+            button8.Location = new Point(0, 40);
+            button8.Name = "button8";
+            button8.Padding = new Padding(35, 0, 0, 0);
+            button8.Size = new Size(250, 40);
+            button8.TabIndex = 1;
+            button8.Text = "button8";
+            button8.TextAlign = ContentAlignment.MiddleLeft;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Dock = DockStyle.Top;
+            button9.FlatAppearance.BorderSize = 0;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.ForeColor = Color.LightGray;
+            button9.Location = new Point(0, 0);
+            button9.Name = "button9";
+            button9.Padding = new Padding(35, 0, 0, 0);
+            button9.Size = new Size(250, 40);
+            button9.TabIndex = 0;
+            button9.Text = "button9";
+            button9.TextAlign = ContentAlignment.MiddleLeft;
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // btnQuanLy
+            // 
+            btnQuanLy.Dock = DockStyle.Top;
+            btnQuanLy.FlatAppearance.BorderSize = 0;
+            btnQuanLy.FlatStyle = FlatStyle.Flat;
+            btnQuanLy.ForeColor = Color.Gainsboro;
+            btnQuanLy.Location = new Point(0, 405);
+            btnQuanLy.Name = "btnQuanLy";
+            btnQuanLy.Padding = new Padding(10, 0, 0, 0);
+            btnQuanLy.Size = new Size(250, 45);
+            btnQuanLy.TabIndex = 4;
+            btnQuanLy.Text = "Quản lý";
+            btnQuanLy.TextAlign = ContentAlignment.MiddleLeft;
+            btnQuanLy.UseVisualStyleBackColor = true;
+            btnQuanLy.Click += btnQuanLy_Click;
+            // 
+            // pnNhapHangSubMenu
+            // 
+            pnNhapHangSubMenu.BackColor = Color.FromArgb(35, 32, 39);
+            pnNhapHangSubMenu.Controls.Add(button5);
+            pnNhapHangSubMenu.Controls.Add(button6);
+            pnNhapHangSubMenu.Dock = DockStyle.Top;
+            pnNhapHangSubMenu.Location = new Point(0, 310);
+            pnNhapHangSubMenu.Name = "pnNhapHangSubMenu";
+            pnNhapHangSubMenu.Size = new Size(250, 95);
+            pnNhapHangSubMenu.TabIndex = 3;
+            // 
+            // button5
+            // 
+            button5.Dock = DockStyle.Top;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.ForeColor = Color.LightGray;
+            button5.Location = new Point(0, 40);
+            button5.Name = "button5";
+            button5.Padding = new Padding(35, 0, 0, 0);
+            button5.Size = new Size(250, 40);
+            button5.TabIndex = 1;
+            button5.Text = "button5";
+            button5.TextAlign = ContentAlignment.MiddleLeft;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Dock = DockStyle.Top;
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.ForeColor = Color.LightGray;
+            button6.Location = new Point(0, 0);
+            button6.Name = "button6";
+            button6.Padding = new Padding(35, 0, 0, 0);
+            button6.Size = new Size(250, 40);
+            button6.TabIndex = 0;
+            button6.Text = "button6";
+            button6.TextAlign = ContentAlignment.MiddleLeft;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // btnNhapHang
+            // 
+            btnNhapHang.Dock = DockStyle.Top;
+            btnNhapHang.FlatAppearance.BorderSize = 0;
+            btnNhapHang.FlatStyle = FlatStyle.Flat;
+            btnNhapHang.ForeColor = Color.Gainsboro;
+            btnNhapHang.Location = new Point(0, 265);
+            btnNhapHang.Name = "btnNhapHang";
+            btnNhapHang.Padding = new Padding(10, 0, 0, 0);
+            btnNhapHang.Size = new Size(250, 45);
+            btnNhapHang.TabIndex = 2;
+            btnNhapHang.Text = "Nhập hàng";
+            btnNhapHang.TextAlign = ContentAlignment.MiddleLeft;
+            btnNhapHang.UseVisualStyleBackColor = true;
+            btnNhapHang.Click += btnNhapHang_Click;
+            // 
+            // pnBanHangSubMenu
+            // 
+            pnBanHangSubMenu.BackColor = Color.FromArgb(35, 32, 39);
+            pnBanHangSubMenu.Controls.Add(button3);
+            pnBanHangSubMenu.Controls.Add(button2);
+            pnBanHangSubMenu.Dock = DockStyle.Top;
+            pnBanHangSubMenu.Location = new Point(0, 170);
+            pnBanHangSubMenu.Name = "pnBanHangSubMenu";
+            pnBanHangSubMenu.Size = new Size(250, 95);
+            pnBanHangSubMenu.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Dock = DockStyle.Top;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.LightGray;
+            button3.Location = new Point(0, 40);
+            button3.Name = "button3";
+            button3.Padding = new Padding(35, 0, 0, 0);
+            button3.Size = new Size(250, 40);
+            button3.TabIndex = 1;
+            button3.Text = "button3";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Top;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.LightGray;
+            button2.Location = new Point(0, 0);
+            button2.Name = "button2";
+            button2.Padding = new Padding(35, 0, 0, 0);
+            button2.Size = new Size(250, 40);
+            button2.TabIndex = 0;
+            button2.Text = "button2";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // btnBanHang
+            // 
+            btnBanHang.Dock = DockStyle.Top;
+            btnBanHang.FlatAppearance.BorderSize = 0;
+            btnBanHang.FlatStyle = FlatStyle.Flat;
+            btnBanHang.ForeColor = Color.Gainsboro;
+            btnBanHang.Location = new Point(0, 125);
+            btnBanHang.Name = "btnBanHang";
+            btnBanHang.Padding = new Padding(10, 0, 0, 0);
+            btnBanHang.Size = new Size(250, 45);
+            btnBanHang.TabIndex = 0;
+            btnBanHang.Text = "Bán hàng";
+            btnBanHang.TextAlign = ContentAlignment.MiddleLeft;
+            btnBanHang.UseVisualStyleBackColor = true;
+            btnBanHang.Click += btnBanHang_Click;
             // 
             // panel1
             // 
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 0);
             panel1.Name = "panel1";
-            panel1.Size = new Size(262, 125);
+            panel1.Size = new Size(250, 125);
             panel1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            button1.Dock = DockStyle.Top;
-            button1.Location = new Point(0, 125);
-            button1.Name = "button1";
-            button1.Size = new Size(262, 45);
-            button1.TabIndex = 0;
-            button1.Text = "Quản lý";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.FromArgb(35, 32, 39);
-            panel2.Controls.Add(button5);
-            panel2.Controls.Add(button4);
-            panel2.Controls.Add(button3);
-            panel2.Controls.Add(button2);
-            panel2.Dock = DockStyle.Top;
-            panel2.Location = new Point(0, 170);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(262, 165);
-            panel2.TabIndex = 1;
-            // 
-            // button2
-            // 
-            button2.Dock = DockStyle.Top;
-            button2.Location = new Point(0, 0);
-            button2.Name = "button2";
-            button2.Size = new Size(262, 40);
-            button2.TabIndex = 0;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Dock = DockStyle.Top;
-            button3.Location = new Point(0, 40);
-            button3.Name = "button3";
-            button3.Size = new Size(262, 40);
-            button3.TabIndex = 1;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            button4.Dock = DockStyle.Top;
-            button4.Location = new Point(0, 80);
-            button4.Name = "button4";
-            button4.Size = new Size(262, 40);
-            button4.TabIndex = 2;
-            button4.Text = "button4";
-            button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            button5.Dock = DockStyle.Top;
-            button5.Location = new Point(0, 120);
-            button5.Name = "button5";
-            button5.Size = new Size(262, 40);
-            button5.TabIndex = 3;
-            button5.Text = "button5";
-            button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -133,19 +288,29 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             pnSideMenu.ResumeLayout(false);
-            panel2.ResumeLayout(false);
+            pnQuanLySubMenu.ResumeLayout(false);
+            pnNhapHangSubMenu.ResumeLayout(false);
+            pnBanHangSubMenu.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel pnSideMenu;
-        private Button button1;
+        private Button btnBanHang;
         private Panel panel1;
-        private Panel panel2;
-        private Button button5;
-        private Button button4;
+        private Panel pnBanHangSubMenu;
         private Button button3;
         private Button button2;
+        private Panel pnQuanLySubMenu;
+        private Button button11;
+        private Button button10;
+        private Button button8;
+        private Button button9;
+        private Button btnQuanLy;
+        private Panel pnNhapHangSubMenu;
+        private Button button5;
+        private Button button6;
+        private Button btnNhapHang;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,51 @@ namespace ModernFlatUI
         public Form1()
         {
             InitializeComponent();
+            customizeDesign();
+        }
+        private void customizeDesign()
+        {
+            pnBanHangSubMenu.Visible = false;
+            pnNhapHangSubMenu.Visible = false;
+            pnQuanLySubMenu.Visible = false;
+        }
+        private void hideSubMenu()
+        {
+            if (pnBanHangSubMenu.Visible == true)
+                pnBanHangSubMenu.Visible = false;
+            if (pnNhapHangSubMenu.Visible == true)
+                pnNhapHangSubMenu.Visible = false;
+            if (pnQuanLySubMenu.Visible == true)
+                pnQuanLySubMenu.Visible = false;
+        }
+        private void showSubMenu(Panel subMenu)
+        {
+            if (subMenu.Visible == false)
+            {
+                hideSubMenu();
+                subMenu.Visible = true;
+            }
+            else
+            {
+                subMenu.Visible = false;
+            }
+        }
+
+        private void btnBanHang_Click(object sender, EventArgs e)
+        {
+            showSubMenu(pnBanHangSubMenu);
+
+
+        }
+
+        private void btnNhapHang_Click(object sender, EventArgs e)
+        {
+            showSubMenu(pnNhapHangSubMenu);
+        }
+
+        private void btnQuanLy_Click(object sender, EventArgs e)
+        {
+            showSubMenu(pnQuanLySubMenu);
         }
     }
 }
